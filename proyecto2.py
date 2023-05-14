b1916d6b057163809975555d8b3ae3bc819ada11
Diego Araya <124745043+diegop19@users.noreply.github.com>
2023-05-14 17:43:22 -0600

    Add files via upload

--- proyecto2.py
+++ proyecto2.py
@@ -6,10 +6,83 @@ from tkinter import ttk
 
 inicioSesion = True
 
-
+ventanaDos = True
+usuarioActual = ""
 
 def ventana_2():
-    print("estas en ventana 2")
+        ventana = tkinter.Tk()  # es la ventana grafica
+        ventana.title("Waze mini")
+        ventana.wm_attributes("-fullscreen", True)   # Fullscreen
+        print(usuarioActual)
+        # Titulo 
+        titulo = tkinter.Label(ventana,text = "Waze",font = ("Impact", 25),bd = 0,padx = 0, pady = 0 ,relief = "groove",fg = "gray50") 
+        mini = tkinter.Label(ventana,text = "mini",font = ("Verdana", 15,"italic"),bd = 0,padx = 0,pady = 0,relief = "groove",fg = "gray45")
+        titulo.place(x=10,y=5)
+        mini.place(x = 85,y = 18)
+        
+        """
+        # Crear la barra de menú
+        menu = tk.Menu(ventana)
+        ventana.config(menu = menu)
+
+        # Crear el elemento del menú
+        archivo = tk.Menu(menu)
+        menu.add_cascade(label = "Opciones",menu = archivo)
+        archivo.add_command(label="Salir",command = lambda: ventana.destroy())
+        """
+        def cargarMapa():
+            print("cargar")
+
+        def seleccionarDestino():
+            print("seleccionar")
+
+        def planificarDestino():
+            print("planificar")
+
+        def guardarDestino():
+            print("guardar")
+
+        def borrarDestino():
+            print("borrar")
+
+        def modificarMapa():
+            print("modificar")
+
+        def salir():
+            salir = messagebox.askyesno(message = "¿Esta seguro?",title = "Salir")
+            if salir:
+                ventana.destroy()
+                
+            
+        #Botones opciones principales
+
+        # Cargar Mapa
+        cargarMapaBoton = tk.Button(ventana, text = "Cargar Mapa",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 55, pady =2, command = cargarMapa).place(x = 25, y = 70)
+        # Seleccionar Destino
+        seleccionarDestinoBoton = tk.Button(ventana, text = "Seleccionar Destino",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 26, pady = 2,command = seleccionarDestino).place(x = 25, y = 115)
+        #Planificar Destino
+        planificarDestinoBoton = tk.Button(ventana, text = "Planificar Destino",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 35, pady = 2,command = planificarDestino).place(x = 25, y = 160)
+        #Guardar Destino
+        guardarDestinoBoton = tk.Button(ventana, text = "Guardar Destino",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 40, pady = 2,command = guardarDestino).place(x = 25, y = 205)
+        #Borrar Destino
+        borrarDestinoBoton = tk.Button(ventana, text = "Borrar Destino",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 48, pady = 2,command = borrarDestino).place(x = 25, y = 250)
+        #Modificar Mapa
+        modificarMapaBoton = tk.Button(ventana, text = "Modificar Mapa",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 46, pady = 2,command = modificarMapa).place(x = 25, y = 295)
+        #Salir
+        salirBoton = tk.Button(ventana, text = "Salir",font = ("Verdana", 12)
+                                    ,bd = 2, bg = "gray30",relief = "flat",fg = "gray92",padx = 90, pady = 2,command = salir).place(x = 25, y = 340)
+
+
+
+
+       
+        ventana.mainloop()
 
 def ventana_1():
     
@@ -20,10 +93,10 @@ def ventana_1():
     ventana.resizable(False,False) # no permite maxmizar la pantalla
     color = tkinter.Label(ventana,bg = "gray60")      # Color Fondo
     color.pack(fill = tkinter.BOTH,expand = True)
-
+     
     # Titulo 
     titulo = tkinter.Label(ventana,text = "Waze",font = ("Impact", 35),bd = 0, bg = "gray60",padx = 0, pady = 0 ,relief = "groove",fg = "white") 
-    mini = tkinter.Label(ventana,text = "mini",font = ("Verdana", 15),bd = 0, bg = "gray60",padx = 0,pady = 0,relief = "groove",fg = "white")
+    mini = tkinter.Label(ventana,text = "mini",font = ("Verdana", 15,"italic"),bd = 0, bg = "gray60",padx = 0,pady = 0,relief = "groove",fg = "white")
     mini.place(x = 470, y = 48)
     titulo.place(x= 365, y = 20)
     """
@@ -32,7 +105,7 @@ def ventana_1():
     etiquetaImagen.place(x = 500,y = 20)
     """
     # Iniciar Sesion 
-    etiquetaInicio = tkinter.Label(ventana,text = "Iniciar Sesión" ,font = ("Verdana", 25,"italic"),bd = 0, bg = "gray60",relief = "groove",fg = "gray92",padx = 0, pady = 0)  
+    etiquetaInicio = tkinter.Label(ventana,text = "Iniciar Sesión" ,font = ("Verdana", 25,"italic"),bd = 0, bg = "gray60",relief = "groove",fg = "gray95",padx = 0, pady = 0)  
     etiquetaInicio.place(x= 100, y= 125)
 
     # Etiqueta de usurario
@@ -52,8 +125,11 @@ def ventana_1():
     ingresarContrasena.place(x= 100, y= 360)
 
     def verificar():
+        # Obtiene los valores ingresado de usuario y contrasena
         
+        global usuarioActual
         usuario_A_verificar = ingresarUsuario.get()
+        usuarioActual = usuario_A_verificar         # guarda el usuario en la variable global
         contrasena_A_verificar = ingresarContrasena.get()
 
         if usuario_A_verificar == "" and contrasena_A_verificar == "":
@@ -92,7 +168,9 @@ def ventana_1():
 if inicioSesion:
     ventana_1()
 
-
+if ventanaDos:
+    ventana_2()
+    
 
 
 
